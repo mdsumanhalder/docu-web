90822a87531cb44838b0a0aaae778606148e4bf6
mdsumanhalder <mdsumanhalder@users.noreply.github.com>
2025-12-25 21:00:53 +0000

    deploy: c31dcb281db1a8a895d5af0153ebeea950fe7597

--- js/menu-wc.js
+++ js/menu-wc.js
@@ -80,6 +80,22 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                             </li>
                 </ul>
                 </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#interfaces-links"' :
+                            'data-bs-target="#xs-interfaces-links"' }>
+                            <span class="icon ion-md-information-circle-outline"></span>
+                            <span>Interfaces</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? ' id="interfaces-links"' : 'id="xs-interfaces-links"' }>
+                            <li class="link">
+                                <a href="interfaces/AppInfo.html" data-type="entity-link" >AppInfo</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/DocumentedProperty.html" data-type="entity-link" >DocumentedProperty</a>
+                            </li>
+                        </ul>
+                    </li>
                         <li class="chapter">
                             <a data-type="chapter-link" href="routes.html"><span class="icon ion-ios-git-branch"></span>Routes</a>
                         </li>
